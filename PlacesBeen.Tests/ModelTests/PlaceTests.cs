064db0988e8ba59be337b7f392bed013a004e502
Mike Manchee <mikemanchee@gmail.com>
2020-09-30 10:01:35 -0700

    add second test to the project - Fail
    Co-authored by: Garrett Brown <gman9mm@live.com>
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>
    Co-Authored-By: Natalie Murphy <Nataliemurphy500@gmail.com>

--- PlacesBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesBeen.Tests/ModelTests/PlaceTests.cs
@@ -11,8 +11,19 @@ namespace PlacesBeen.Tests
     [TestMethod]
     public void Place_InstantiateAnInstanceOfPlace_Place()
     {
-      Place newPlace = new Place();
+      Place newPlace = new Place("test");
       Assert.AreEqual(typeof(Place), newPlace.GetType());
+    }
+    [TestMethod]
+    public void Place_InstantiateAnInstanceOfPlace_newPlace()
+    {
+      //Arrange
+      string cityName = "Amsterdam";
+      //Act
+      Place newPLace = new Place(cityName);
+      string result = newPLace.CityName;
+      // Assert
+      Assert.AreEqual("Amsterdam", result);
     }
   }
 }
--- PlacesBeen/Models/Place.cs
+++ PlacesBeen/Models/Place.cs
@@ -4,9 +4,12 @@ namespace PlacesBeen.Models
 {
   public class Place
   {
-    public Place()
+    public string CityName { get; set; }
+    // public string Description { get; set; }
+    // public int Id { get; }
+    public Place(string cityName)
     {
-
+      
     }
   }
 }
